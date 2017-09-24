48d73cf42090ce61fd4d51c4f47f619d5fa4048f
GaryPush <push05018117@gmail.com>
2017-09-24 04:31:50 -0400

    Update twitter-streaming.py

--- twitter-streaming.py
+++ twitter-streaming.py
@@ -4,42 +4,74 @@ import json
 import tweepy
 from tweepy import Stream
 from tweepy.streaming import StreamListener
-import time
+import datetime
 
-consumer_key= 'CvL60UbrszIbU7sPabRNsVis0'
-consumer_secret= 'OOU3EOonHMqOfaPZ7SGRYucP2wxDpHv5l3L0TKcyV09Oxw0yu3'
-access_token= '2374450932-EFnX9Rt07h4E1PWwj4lNyeunvF2KN3ahsqC5c3g'
-access_token_secret= 'DQfcife9i7teD2DtWtJQOYlasK0yByqbkV7MR903AW2Zc'
-limit = 5
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('TSA')
+class Listener (StreamListener):
 
-class listener (StreamListener):
-
-    def __init__(self):
+    def __init__(self, table):
         StreamListener.__init__(self)
-        self.count = 0
+        self.table = table
 
     def on_data(self, data):
         try:
-            self.write(data)
-        except ValueError, e:
-            print("Failed on data", e)
-            time.sleep(5)
-        self.count += 1
-        if self.count >= limit:
-            exit()
+            self.table.insert_tweet(Tweet(data))
+        except Exception, e:
+            self.table.insert_log(Log(str(e)))
 
     def on_error(self, status):
-        print(status)
+        self.table.insert_log(Log(str(status)))
 
-    def write(self, data):
-        tweet = json.loads(data)
-        date, text = tweet['created_at'], tweet['text']
-        print(date, text)
-        table.put_item(
+
+class DataTable:
+
+    def __init__(self, tweets, logs):
+        self.tweets = tweets
+        self.logs = logs
+
+    def insert_tweet(self, tweet):
+        self.tweets.put_item(
             Item={
-                'date': date,
-                'text': text,
+                'Date': tweet.time_stamp,
+                'Text': tweet.text,
             }
         )
+
+    def insert_log(self, log):
+        self.logs.put_item(
+            Item={
+                'Date': log.time_stamp,
+                'Message': log.message,
+            }
+        )
+
+
+class Log:
+
+    def __init__(self, message):
+        self.message = message
+        self.time_stamp = str(datetime.datetime.utcnow())
+
+
+class Tweet:
+
+    def __init__(self, json_data):
+        tweet = json.loads(json_data)
+        self.time_stamp, self.text = tweet['created_at'], tweet['text']
+        print(self.text)
+
+
+if __name__ == '__main__':
+    KEY_WORD = 'Car'
+
+    consumer_key= 'CvL60UbrszIbU7sPabRNsVis0'
+    consumer_secret= 'OOU3EOonHMqOfaPZ7SGRYucP2wxDpHv5l3L0TKcyV09Oxw0yu3'
+    access_token= '2374450932-EFnX9Rt07h4E1PWwj4lNyeunvF2KN3ahsqC5c3g'
+    access_token_secret= 'DQfcife9i7teD2DtWtJQOYlasK0yByqbkV7MR903AW2Zc'
+
+    dynamodb = boto3.resource('dynamodb')
+    table = DataTable(dynamodb.Table('Tweet'), dynamodb.Table('Log'))
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    twitterStream = Stream(auth, Listener(table))
+    twitterStream.filter(track=[KEY_WORD])
